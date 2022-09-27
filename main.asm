0e12b4c29d0309ce3da71cceff088cccb032e8de
lplume <208442+lplume@users.noreply.github.com>
2022-09-27 13:34:49 +0200

    fixes

--- main.asm
+++ main.asm
@@ -8,6 +8,39 @@
 ;END OF COMMON HEADER
 
 	;start of code $100A
+	lda #0
+	lda #>HelloWorld		;Load address of message into Zeropage $20/1
+	sta $21
+	lda #<HelloWorld
+	sta $20
+	
+	jsr PrintStr			;ShowString routine
+	jsr Newline
+	jsr Newline
+	
+	
+	rts						;Return to basic
+	
+
+HelloWorld:
+	db "Hello World",255
+	
+PrintStr:
+        ldy #0				;Reset Y
+PrintStr_again:
+        lda ($20),y		;Read in a character
+        
+		cmp #255
+        beq PrintStr_Done	;Return if we've reached a 255
+        
+		jsr PrintChar		;Print to screen if not
+        iny
+        jmp PrintStr_again	;repeat
+PrintStr_Done:
+        rts					;Return when done					
+	
+PrintCharOK:
+	jmp $ffd2
 	
 ;COMMON FOOTER	
 	include "include/Monitor.asm"       			;Debugging tools
